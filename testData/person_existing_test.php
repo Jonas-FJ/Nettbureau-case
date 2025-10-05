38f398f548d327236e0c39ac1c08c359646eaeb7
Jonas-FJ <37621459+Jonas-FJ@users.noreply.github.com>
2025-10-05 17:20:32 +0200

    Update person_existing_test.php

--- testData/person_existing_test.php
+++ testData/person_existing_test.php
@@ -1,13 +1,13 @@
 <?php
 return 
 [
-    "name" => "Leia Organa",
+    "name" => "Obi-Wan Kenobi",
     "phone" => "12345678",
     "email" => "ola.nordmannn@online.no",
-    "housing_type" => "Leilighet",
+    "housing_type" => "Rekkehus",
     "property_size" => 160,
-    "deal_type" => "Fastpris",
-    "contact_type" => "Bedrift"
+    "deal_type" => "Kraftforvaltning",
+    "contact_type" => "Borettslag"
 ];
 
 ?>
